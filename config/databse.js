ba4a22551dc9ab6154484730038192dd8086839b
Manishsahusahu <sahumanish327@gmail.com>
2023-08-24 13:35:35 +0530

    userSchema is defined

---
+++ config/databse.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+exports.connect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((con) =>
+      console.log(`Connection successfully connected to ${con.connection.host}`)
+    )
+    .catch((error) => {
+      console.log(error.message);
+      process.exit(1);
+    });
+};
