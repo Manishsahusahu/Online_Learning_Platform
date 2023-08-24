e141c9cff6466987a10a2ea6ceb11c25c5bfff9f
Manishsahusahu <sahumanish327@gmail.com>
2023-08-24 15:25:44 +0530

    sending mail functionality

--- models/otp.model.js
+++ models/otp.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const otpSchema = new mongoose.Schema(
     {
@@ -13,10 +14,28 @@ const otpSchema = new mongoose.Schema(
         createdAt: {
             type: Date,
             default: Date.now(),
-            expires: 5 * 60 * 1000, 
+            expires: 5 * 60 * 1000,
         },
     },
     { timestamps: true }
 );
+
+const sendVerificationEmail = async (email, otp) => {
+    try {
+        const response = await mailSender(
+            email,
+            "Verification Email from Online Education",
+            otp
+        );
+        console.log("Mail has been sent successfully", response);
+    } catch (error) {
+        console.log("Error occured while sending mail ", error);
+    }
+};
+
+otpSchema.pre("save", async function (next) {
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+});
 
 module.exports = mongoose.model("OTP", otpSchema);
