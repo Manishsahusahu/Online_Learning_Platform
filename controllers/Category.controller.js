3a2f12b8d3dbb72f92629d7b5246826c7493bbca
Manishsahusahu <sahumanish327@gmail.com>
2023-08-26 20:24:42 +0530

    additional controllers for categories of courses

--- controllers/Category.controller.js
+++ controllers/Category.controller.js
@@ -39,3 +39,36 @@ exports.allCategories = async (req, res, next) => {
         return next(new AppError(error.message, 500));
     }
 };
+
+exports.categoryPageDetails = async function (req, res, next) {
+    try {
+        const { categoryId } = req.body;
+        if (!categoryId)
+            return next(new AppError("all fields are mandatory", 500));
+
+        const selectedCategory = await Category.findById(categoryId)
+            .populate("courses")
+            .exec();
+
+        if (!selectedCategory)
+            return next(new AppError("Category not found", 500));
+
+        const differentCategories = await Category.find({
+            _id: { $ne: categoryId },
+        })
+            .populate("courses")
+            .exec();
+
+        // home work for top selling courses findings
+
+        return res.status(200).json({
+            success: true,
+            data: {
+                selectedCategory,
+                differentCategories,
+            },
+        });
+    } catch (error) {
+        return next(new AppError(error.message, 500));
+    }
+};
